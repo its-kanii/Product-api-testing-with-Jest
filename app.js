7174f7a33f4e03ff7425bc1ab2098bf2ebb9e105
its-kanii <Kanimeena678@gmail.com>
2025-06-29 08:01:35 +0530

    Fix PORT error in app.js and update Keploy GitHub Actions workflow

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0'; 
 
 app.use(cors());
 app.use(express.json());
